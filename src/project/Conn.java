d22fef85f79de420e7bc2cc19eb6d7637ce21cdb
Suyash Soni <suyashsoni93@gmail.com>
2026-04-02 21:38:08 +0530

    Initial commit - SRGI Student and Faculty Management System

--- src/project/Conn.java
+++ src/project/Conn.java
@@ -10,7 +10,7 @@ public class Conn {
 	Conn() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			c = DriverManager.getConnection("jdbc:mysql:///srgistudentandfacultymanagementsystem", "root", "Suyash@1221");
+			c = DriverManager.getConnection("jdbc:mysql:///srgistudentandfacultymanagementsystem", "root", "password_here");
 			s = c.createStatement();
 		} catch (Exception e) {
 			e.printStackTrace();
